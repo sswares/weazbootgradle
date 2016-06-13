f7b869c782971424d5e5606bdae6bfc2f1866e72
Walker Berry <wberry@corelogic.com>
2016-06-13 11:30:37 -0700

    Cleanup

--- server-auth/src/main/java/net/weaz/AuthApplication.java
+++ server-auth/src/main/java/net/weaz/AuthApplication.java
@@ -99,8 +99,7 @@ public class AuthApplication extends WebMvcConfigurerAdapter {
 
     @Configuration
     @EnableAuthorizationServer
-    protected static class OAuth2AuthorizationConfig extends
-            AuthorizationServerConfigurerAdapter {
+    protected static class OAuth2AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
 
         @Autowired
         private AuthenticationManager authenticationManager;
@@ -121,8 +120,8 @@ public class AuthApplication extends WebMvcConfigurerAdapter {
                     .withClient("acme")
                     .secret("acmesecret")
                     .autoApprove(true)
-                    .authorizedGrantTypes("authorization_code", "refresh_token",
-                            "password").scopes("openid");
+                    .authorizedGrantTypes("authorization_code", "refresh_token", "password")
+                    .scopes("openid");
         }
 
         @Override
@@ -135,8 +134,7 @@ public class AuthApplication extends WebMvcConfigurerAdapter {
         @Override
         public void configure(AuthorizationServerSecurityConfigurer oauthServer)
                 throws Exception {
-            oauthServer.tokenKeyAccess("permitAll()").checkTokenAccess(
-                    "isAuthenticated()");
+            oauthServer.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
         }
 
         private static class EnhancedJwtAccessTokenConverter extends JwtAccessTokenConverter {
