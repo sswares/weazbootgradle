72509737d0f62473bcd9270f8473df393d9f8561
Walker Berry <walker@weaz.net>
2016-08-03 15:33:07 -0700

    Removing old csrf cookie filter in leu of the spring 4.1 factory method.  CLEANER

--- server-main/src/main/java/net/weaz/MainApplication.java
+++ server-main/src/main/java/net/weaz/MainApplication.java
@@ -9,20 +9,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfFilter;
-import org.springframework.security.web.csrf.CsrfToken;
-import org.springframework.security.web.csrf.CsrfTokenRepository;
-import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
-import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.util.WebUtils;
-
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -30,11 +18,15 @@ import java.io.IOException;
 @SuppressWarnings("SpringJavaAutowiringInspection")
 public class MainApplication extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private ResourceServerTokenServices tokenServices;
+    private final ResourceServerTokenServices tokenServices;
+
+    private final OAuth2ClientContext clientContext;
 
     @Autowired
-    private OAuth2ClientContext clientContext;
+    public MainApplication(ResourceServerTokenServices tokenServices, OAuth2ClientContext clientContext) {
+        this.tokenServices = tokenServices;
+        this.clientContext = clientContext;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
@@ -46,38 +38,8 @@ public class MainApplication extends WebSecurityConfigurerAdapter {
                 .and().antMatcher("/**").authorizeRequests()
                 .antMatchers("/partials/**", "/", "/login").permitAll()
                 .anyRequest().authenticated().and().csrf()
-                .csrfTokenRepository(csrfTokenRepository())
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 .and()
-                .addFilterBefore(new BearerTokenAccessFilter(tokenServices, clientContext), CsrfFilter.class)
-                .addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);
-    }
-
-    private Filter csrfHeaderFilter() {
-        return new OncePerRequestFilter() {
-            @Override
-            protected void doFilterInternal(HttpServletRequest request,
-                                            HttpServletResponse response, FilterChain filterChain)
-                    throws ServletException, IOException {
-                CsrfToken csrf = (CsrfToken) request
-                        .getAttribute(CsrfToken.class.getName());
-                if (csrf != null) {
-                    Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
-                    String token = csrf.getToken();
-                    if (cookie == null
-                            || token != null && !token.equals(cookie.getValue())) {
-                        cookie = new Cookie("XSRF-TOKEN", token);
-                        cookie.setPath("/");
-                        response.addCookie(cookie);
-                    }
-                }
-                filterChain.doFilter(request, response);
-            }
-        };
-    }
-
-    private CsrfTokenRepository csrfTokenRepository() {
-        HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
-        repository.setHeaderName("X-XSRF-TOKEN");
-        return repository;
+                .addFilterBefore(new BearerTokenAccessFilter(tokenServices, clientContext), CsrfFilter.class);
     }
 }
